e91d81ba52bbeb982b6f5985aca4d4605d9d4c63
Lars van Arragon <lars@arragone.nl>
2019-09-25 16:13:17 +0200

    Added Hide/Show Python code button to be baked into the HTML when using nbconv.sh. Added more content to collections and channels. Created install and run guide

--- tools/convnb.sh
+++ tools/convnb.sh
@@ -6,15 +6,21 @@ JQUERY_SOURCE="https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/3.4.1\/jquery.
 # The following is the code for the continouous MathJax updates, could be extracted to a file and loaded
 MATHJAX_JS="\t<script type=\"text\/javascript\">\n\t\t\$\(\'body\'\).on(\"DOMSubtreeModified\", function\(\) \{\n\t\t\tMathJax.Hub.Queue\(\[\"Typeset\", MathJax.Hub, this\]\)\;\n\t\t\}\)\;\n\t<\/script>"
 
+# The dir where convnb.sh is located
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+TEMPLATE_FOLDER="templates"
+
+printf "Called convnb.sh in directory: ${DIR}\n"
+
 if [ $# -eq 0 ]; then
-    printf "To convert a notebook please supply a correct path to it"
+    printf "To convert a notebook please supply a correct path to it\n"
     exit 1
 fi
 
 if [[ $* == *--help* ]]; then
     printf "convnb is simple script to convert a Jupyter Notebook to HTML using the nbinteract library\n"
     printf "It adds several handy tools to the HTML like jQuery and the ability for MathJax to rerender your interactive Latex\n\n"
-    printf "convnb must be called as follows:\n\tconvnb.sh path/to/notebook.ipynb"
+    printf "convnb must be called as follows:\n\tconvnb.sh path/to/notebook.ipynb\n"
     exit 0
 fi
 
@@ -33,6 +39,15 @@ fi
 
 if [[ !($NOTEBOOK =~ \.ipynb$) ]]; then
     printf "The file specified is not an '.ipynb' file\n"
+    exit 1
+fi
+
+printf "Loading template data from ${DIR}/${TEMPLATE_FOLDER}\n"
+
+# Load in template html for hide code button
+HCB_TEMPLATE_FILE="${DIR}/${TEMPLATE_FOLDER}/hide_code_button.html"
+if [ ! -f "$HCB_TEMPLATE_FILE" ]; then
+	printf "The template file ${HCB_TEMPLATE_FILE} is missing, please redownload them from GitHub\n"
     exit 1
 fi
 
@@ -60,5 +75,10 @@ if [ $? -ne 0 ]; then
     printf "Error while adding MathJax features to ${NB_BASE}.html\n"
     exit 1
 fi
+
+printf "Adding template data to ${NB_BASE}.html\n"
+
+sed -i -e "/<body>/r ${HCB_TEMPLATE_FILE}" ${NB_BASE}.html
+# sed -i -e "s/<body>/<body>\n${HCB_TEMPLATE}\n/g" ${NB_BASE}.html
 
 printf "Successfully converted $NOTEBOOK to HTML"
